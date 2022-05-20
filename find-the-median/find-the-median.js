51d2b2ed6e5c59ad74417404958c20e4442b164d
Mushfiqur Niloy <mushfiqur06@gmail.com>
2022-05-20 23:54:56 +0600

    Solve the problem of find the median number

---
+++ find-the-median/find-the-median.js
@@ -0,0 +1,25 @@
+function quickSort(items){
+    if(items.length <= 1) return items;
+
+    const pivot = items[items.length - 1];
+    const leftArr = []
+    const rightArr = []
+
+    for(let i = 0; i < items.length - 1; i++){
+        if(items[i] <= pivot){
+            leftArr.push(items[i])
+        }else{
+            rightArr.push(items[i])
+        }
+    }
+
+    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)]
+}
+
+function find_middle(arr){
+    let arrs = quickSort(arr);
+    console.log(arrs)
+    return arrs[Math.floor(arrs.length / 2)]
+}
+
+console.log(find_middle([1, 4, 2, 6, 7, 3, 8]))
