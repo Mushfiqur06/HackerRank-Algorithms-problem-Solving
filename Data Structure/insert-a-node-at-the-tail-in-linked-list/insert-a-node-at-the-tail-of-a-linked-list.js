b880f1ecb83a63e8b4c64488cec2a6a1a1f28bb6
Mushfiqur Niloy <mushfiqur06@gmail.com>
2022-06-05 23:12:25 +0600

    Solve the problem

---
+++ Data Structure/insert-a-node-at-the-tail-in-linked-list/insert-a-node-at-the-tail-of-a-linked-list.js
@@ -0,0 +1,20 @@
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function insertNodeAtTail(head, data) {
+    if(head === null) return new SinglyLinkedListNode(data);
+    let temp = head;
+    while(temp.next){
+        temp = temp.next
+    }
+    temp.next = new SinglyLinkedListNode(data)
+    
+return head;
+
+}
